b435bec2a1b4aba50aa8e7015e426f9f0a590f0d
Luis Canada <canadaluis5@gmail.com>
2022-06-28 08:52:40 -0400

    Finish file system

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,17 +1,24 @@
 import {
+  IonCol,
   IonContent,
   IonFab,
   IonFabButton,
+  IonGrid,
   IonHeader,
   IonIcon,
+  IonImg,
   IonPage,
+  IonRow,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
 import { camera } from "ionicons/icons";
+import { usePhotoGallery } from "../hooks/usePhotoGallery";
 import "./Tab2.css";
 
 const Tab2: React.FC = () => {
+  const { photos, takePhoto } = usePhotoGallery();
+
   return (
     <IonPage>
       <IonHeader>
@@ -20,6 +27,15 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
         <IonFab vertical="bottom" horizontal="end" slot="fixed">
           <IonFabButton onClick={() => takePhoto()}>
             <IonIcon icon={camera} />
@@ -28,10 +44,6 @@ const Tab2: React.FC = () => {
       </IonContent>
     </IonPage>
   );
-
-  function takePhoto() {
-    // Do nothing
-  }
 };
 
 export default Tab2;
