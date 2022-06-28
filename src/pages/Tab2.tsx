70e9307d6aaf240f278d5d9a48e20ff37e0c3e64
Luis Canada <canadaluis5@gmail.com>
2022-06-28 08:12:41 -0400

    Finish quick start

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,25 +1,37 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import {
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { camera } from "ionicons/icons";
+import "./Tab2.css";
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera} />
+          </IonFabButton>
+        </IonFab>
       </IonContent>
     </IonPage>
   );
+
+  function takePhoto() {
+    // Do nothing
+  }
 };
 
 export default Tab2;
